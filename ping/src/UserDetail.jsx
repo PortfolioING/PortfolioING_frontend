7e24effd134642810e77153f2ae8d947333aa2cb
seanlancekim0 <122666778+seanlancekim0@users.noreply.github.com>
2024-11-18 21:58:46 +0900

    [UPDATE] response로 받은 data 값을 사용하려면 각각의 객체를 식별할 수 있도록 key값을 고유한 user의 id로 설정해줘야함.

--- ping/src/UserDetail.jsx
+++ ping/src/UserDetail.jsx
@@ -1,10 +1,14 @@
-import { useEffect } from "react";
+import { useState, useEffect } from 'react';
 import APICall from "./apis/instance";
 
 export default function UserDetail({ userId }) {
+
+  const [user, setUser] = useState('');
+
   useEffect(() => {
     APICall.get(`/api/auth/${userId}`) // '/api'로 시작하여 프록시가 동작하도록 설정
       .then((response) => {
+        setUser(response.data);
         console.log(response.data);
       })
       .catch((error) => {
@@ -12,5 +16,11 @@ export default function UserDetail({ userId }) {
       });
   }, [userId]);
 
-  return <div>유저 상세 정보</div>;
+  return <div key={user.userId}>
+          <h2>유저 상세 정보</h2>
+          <p><strong>유저 이름: </strong>{user.name}</p>
+          <p><strong>이메일: </strong>{user.email}</p>
+          <p><strong>닉네임: </strong>{user.nickname}</p>
+          <p><strong>프사 url: </strong>{user.profilePic}</p>
+  </div>;
 }
