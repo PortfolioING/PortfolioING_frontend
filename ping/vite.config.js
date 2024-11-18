2f5294bd7bdd70ea1394139e4d1a7881ab3d513d
hamxxn <hamin031121@gmail.com>
2024-11-18 16:48:16 +0900

    갓순대

--- ping/vite.config.js
+++ ping/vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080', // Spring Boot API 서버 주소
+      "/api": {
+        target: "http://localhost:8080", // Spring Boot API 서버 주소
         changeOrigin: true,
-      }
-    }
-  }
-})
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
