c2267516841e5e8a40423b5d1e5d0be53135644d
hamxxn <hamin031121@gmail.com>
2024-11-26 17:27:10 +0900

    템플릿 선택 후, 템플릿에서 해당 survey 불러오기

--- ping/src/apis/postSurvey.js
+++ ping/src/apis/postSurvey.js
@@ -1,22 +1,20 @@
 import instance from "./instance";
-
 const PostSurvey = async (surveyData, projectIds) => {
-    const requestData = {
-      name: surveyData.name,
-      introduce: surveyData.introduction,
-    //   profile: surveyData.photo, // 이미지 URL로 변환 필요
-      profile: "photo",
-      projectsId: projectIds
-    };
+  const requestData = {
+    name: surveyData.name,
+    introduce: surveyData.introduction,
+    profile: "photo", // 실제 이미지 URL로 대체 필요
+    projectsId: projectIds,
+  };
 
-    console.log("Survey POST request body", requestData);
+  console.log("PostSurvey 요청 데이터:", requestData); // 디버깅용 로그
+
   try {
-
     const response = await instance.post("api/survey", requestData);
     return response.data;
-
   } catch (error) {
     if (error.response) {
+      console.error("서버 응답 오류:", error.response.data); // 서버의 오류 메시지 로그
       if (error.response.status === 401) {
         alert("잘못된 요청입니다.");
       } else if (error.response.status === 500) {
@@ -28,5 +26,4 @@ const PostSurvey = async (surveyData, projectIds) => {
     throw error;
   }
 };
-
 export default PostSurvey;
--- ping/src/apis/putTemplate.js
+++ ping/src/apis/putTemplate.js
@@ -1,28 +1,31 @@
 import instance from "./instance";
 
 const PutTemplate = async (templateId) => {
-    const requestData = {
-        templateId: templateId
-    };
+  const requestData = {
+    templateId: templateId,
+  };
 
-    console.log("Portfolio PUT Request Body", requestData);
+  console.log("Portfolio PUT Request Body", requestData);
 
-    try {
-        const portfolioId = sessionStorage.getItem("portfolioId");
-        const response = await instance.put(`api/portfolio/template/${portfolioId}`, requestData);
-        return response.data;
-    } catch (error) {
-        if (error.response) {
-            if (error.response.status === 401) {
-                alert("잘못된 요청입니다.");
-            } else if (error.response.status === 500) {
-                alert("Portfolio를 업데이트할 수 없습니다.");
-            } else {
-                alert("잠시 후 다시 시도해주세요.");
-            }
-        }
-        throw error;
+  try {
+    const portfolioId = sessionStorage.getItem("portfolioId");
+    const response = await instance.put(
+      `api/portfolio/template/${portfolioId}`,
+      requestData
+    );
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      if (error.response.status === 401) {
+        alert("잘못된 요청입니다.");
+      } else if (error.response.status === 500) {
+        alert("Portfolio를 업데이트할 수 없습니다.");
+      } else {
+        alert("잠시 후 다시 시도해주세요.");
+      }
     }
+    throw error;
+  }
 };
 
 export default PutTemplate;
